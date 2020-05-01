0fb41fdb035e42c46f7c78239cf15d33965b8272
tabbykhan <tabishskhn@gmail.com>
2020-05-01 18:41:40 +0530

    first commit by azhar

--- app/src/main/java/com/imuons/pmcindia/view/SignupActivity.java
+++ app/src/main/java/com/imuons/pmcindia/view/SignupActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;

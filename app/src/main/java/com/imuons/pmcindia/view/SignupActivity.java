fe34cd411847db76787a490086d8635905bc4561
tabbykhan <tabishskhn@gmail.com>
2020-05-01 18:44:20 +0530

    first commit by azhar

--- app/src/main/java/com/imuons/pmcindia/view/SignupActivity.java
+++ app/src/main/java/com/imuons/pmcindia/view/SignupActivity.java
@@ -13,8 +13,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.imuons.pmcindia.DataModel.LoginData;
+import com.imuons.pmcindia.Entity.LoginEntity;
 import com.imuons.pmcindia.Entity.RegitrationEntity;
 import com.imuons.pmcindia.R;
+import com.imuons.pmcindia.ResponseModel.LoginResponse;
 import com.imuons.pmcindia.ResponseModel.QuestionResponse;
 import com.imuons.pmcindia.ResponseModel.RegisterResponse;
 import com.imuons.pmcindia.ResponseModel.RendomNumberResponse;
@@ -64,7 +67,7 @@ public class SignupActivity extends Activity {
         tv_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                Intent intent = new Intent(SignupActivity.this, SignupActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -164,10 +167,11 @@ public class SignupActivity extends Activity {
                     if (authResponse != null) {
                         Log.i("RendomResponse::", new Gson().toJson(authResponse));
                         if (authResponse.getCode() == 200) {
-                            Intent intent = new Intent();
+                           /* Intent intent = new Intent();
                             intent.putExtra("userId" , authResponse.getResponseData().getUserid());
                             setResult(180 , intent);
-                            finish();
+                            finish();*/
+                           callLoginApi(new LoginEntity(regitrationEntity.getUser_id() , regitrationEntity.getPassword()));
                         } else {
                             Toast.makeText(SignupActivity.this, authResponse.getMessage(), Toast.LENGTH_SHORT).show();
                         }
@@ -191,6 +195,58 @@ public class SignupActivity extends Activity {
             progressBar.setVisibility(View.GONE);
             Toast.makeText(this, "Please check your internet", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void callLoginApi(LoginEntity loginEntity) {
+        if (AppCommon.getInstance(this).isConnectingToInternet(this)) {
+            AppCommon.getInstance(this).setNonTouchableFlags(this);
+            AppService apiService = ServiceGenerator.createService(AppService.class);
+            progressBar.setVisibility(View.VISIBLE);
+            //  Call call = apiService.token_CALL(new AuthEntitiyClass("vp235345@vp11.com", "123456"));
+            Call call = apiService.CallLogin(loginEntity);
+            call.enqueue(new Callback() {
+                @Override
+                public void onResponse(Call call, Response response) {
+                    AppCommon.getInstance(SignupActivity.this).clearNonTouchableFlags(SignupActivity.this);
+                    progressBar.setVisibility(View.GONE);
+                    LoginResponse authResponse = (LoginResponse) response.body();
+                    if (authResponse != null) {
+                        Log.i("loginResponse::", new Gson().toJson(authResponse));
+                        if (authResponse.getCode() == 200) {
+                            if(authResponse.getData() != null)
+
+                                setData(authResponse.getData());
+
+                        } else {
+                            Toast.makeText(SignupActivity.this, authResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        AppCommon.getInstance(SignupActivity.this).showDialog(SignupActivity.this, "Server Error");
+                    }
+                }
+
+                @Override
+                public void onFailure(Call call, Throwable t) {
+                    progressBar.setVisibility(View.GONE);
+                    AppCommon.getInstance(SignupActivity.this).clearNonTouchableFlags(SignupActivity.this);
+                    // loaderView.setVisibility(View.GONE);
+                    Toast.makeText(SignupActivity.this, "Server Error", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+        } else {
+            // no internet
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(this, "Please check your internet", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setData(LoginData data) {
+        AppCommon.getInstance(this).setUserObject(new Gson().toJson(data));
+        AppCommon.getInstance(this).setToken(data.getAccess_token());
+        startActivity(new Intent(this , DashboardActivity.class) );
+        finish();
     }
 
 

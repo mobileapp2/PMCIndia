1d3364bd1ea0b60ea59259f0a3c1a8441e9df092
Rahul Vhayaskar <r.vhayaskar@adlersolutions.in>
2020-05-04 15:20:07 +0530

    imageloader

--- app/src/main/java/com/imuons/pmcindia/fragments/FragmentInvestment.java
+++ app/src/main/java/com/imuons/pmcindia/fragments/FragmentInvestment.java
@@ -398,13 +398,13 @@ public class FragmentInvestment extends Fragment implements InvestmentGridAdapte
                     getActivity().startActivityForResult(galleryIntent, GALLERY_PICTURE_REQUEST_CODE);
                 } catch (Exception e) {
                     e.printStackTrace();
-                    Toast.makeText(getContext(),"Something went Wrong", Toast.LENGTH_LONG).show();
+//                    Toast.makeText(getContext(),"Something went Wrong", Toast.LENGTH_LONG).show();
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
-            Toast.makeText(getContext(),"Storage area Something went Wrong "+e.getMessage(),
-                    Toast.LENGTH_LONG).show();
+//            Toast.makeText(getContext(),"Storage area Something went Wrong "+e.getMessage(),
+//                    Toast.LENGTH_LONG).show();
         }
     }
 
@@ -488,9 +488,10 @@ public class FragmentInvestment extends Fragment implements InvestmentGridAdapte
                 Log.d(tag, "----req permission-GALLERY_PICTURE_REQUEST_CODE--" + fileUri);
                 galleryIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
                 getActivity().startActivityForResult(galleryIntent, GALLERY_PICTURE_REQUEST_CODE);
-                Toast.makeText(getContext(),"Gallery Permission Allow", Toast.LENGTH_LONG).show();
+                //Toast.makeText(getContext(),"Gallery Permission Allow", Toast.LENGTH_LONG).show();
             }else{
-                Toast.makeText(getContext(),"Gallery Permission Denied", Toast.LENGTH_LONG).show();
+               // Toast.makeText(getContext(),"Gallery Permission Denied", Toast.LENGTH_LONG)
+                // .show();
             }
         } else if (requestCode == INITIAL_REQUEST_CAMERA) {
             int result = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA);
